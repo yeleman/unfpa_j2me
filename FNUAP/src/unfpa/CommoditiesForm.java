7544c548832066b3b570f00559939dda836a4897
Fadiga <ibfadiga@gmail.com>
2012-04-04 15:10:07 +0000

    formulaire ui CommoditiesForm

--- FNUAP/src/unfpa/CommoditiesForm.java
+++ FNUAP/src/unfpa/CommoditiesForm.java
@@ -13,14 +13,13 @@ import unfpa.HelpForm.*;
  * Sends as SMS
  * @author fad
  */
+
 public class CommoditiesForm extends Form implements CommandListener {
 
     private static final Command CMD_EXIT = new Command ("Retour",
                                                             Command.BACK, 1);
     private static final Command CMD_SEND = new Command ("Envoi.",
                                                               Command.OK, 1);
-    private static final Command CMD_SAVE = new Command ("Enregistrer.",
-                                                              Command.OK, 2);
     private static final Command CMD_HELP = new Command ("Aide",
                                                             Command.HELP, 3);
 
@@ -29,38 +28,65 @@ public class CommoditiesForm extends Form implements CommandListener {
     private Configuration config;
 
     private String ErrorMessage = "";
-    private static final String[] choice = {"OUI", "NON"};
+    private static final String[] choice = {"NON", "OUI"};
+    private static final String[] year_list = {"2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"};
+    private static final String[] yesnoavail = {"Non", "Oui. Fournitures disponibles", "Oui. Mais non disponibles"};
+    private static final String month_list[] = {"----", "(01)Jan","(2)Feb","(3)Mar","(4)Apr","(5)May","(6)Jun","(7)Jul","(8)Aug","(9)Sep","(10)Oct","(11)Nov","(12)Dec"};
 
     //General Informatien
-    private TextField reporting_year;
-    private TextField reporting_month;
+    private ChoiceGroup reporting_year;
+    private ChoiceGroup reporting_month;
     private TextField location_of_sdp;
-    private TextField family_planning;
-    private TextField delivery_services;
+    private ChoiceGroup family_planning;
+    private ChoiceGroup delivery_services;
     private ChoiceGroup male_condom;
+    private TextField nb_male_condom;
     private ChoiceGroup female_condom;
+    private TextField nb_female_condom;
     private ChoiceGroup oral_pills;
+    private TextField nb_oral_pills;
     private ChoiceGroup injectable;
+    private TextField nb_injectable;
     private ChoiceGroup iud;
+    private TextField nb_iud;
     private ChoiceGroup implants;
+    private TextField nb_implants;
     private ChoiceGroup female_sterilization;
+    private TextField nb_female_sterilization;
     private ChoiceGroup male_sterilization;
+    private TextField nb_male_sterilization;
     private ChoiceGroup amoxicillin_ij;
+    private TextField nb_amoxicillin_ij;
     private ChoiceGroup amoxicillin_cap_gel;
+    private TextField nb_amoxicillin_cap_gel;
     private ChoiceGroup amoxicillin_suspension;
+    private TextField nb_amoxicillin_suspension;
     private ChoiceGroup azithromycine_tab;
+    private TextField nb_azithromycine_tab;
     private ChoiceGroup azithromycine_suspension;
+    private TextField nb_azithromycine_suspension;
     private ChoiceGroup benzathine_penicillin;
+    private TextField nb_benzathine_penicillin;
     private ChoiceGroup cefexime;
+    private TextField nb_cefexime;
     private ChoiceGroup clotrimazole;
+    private TextField nb_clotrimazole;
     private ChoiceGroup ergometrine_tab;
+    private TextField nb_ergometrine_tab;
     private ChoiceGroup ergometrine_vials;
+    private TextField nb_ergometrine_vials;
     private ChoiceGroup iron;
+    private TextField nb_iron;
     private ChoiceGroup folate;
+    private TextField nb_folate;
     private ChoiceGroup iron_folate;
+    private TextField nb_iron_folate;
     private ChoiceGroup magnesium_sulfate;
+    private TextField nb_magnesium_sulfate;
     private ChoiceGroup metronidazole;
+    private TextField nb_metronidazole;
     private ChoiceGroup oxytocine;
+    private TextField nb_oxytocine;
 
 
 public CommoditiesForm(UNFPAMIDlet midlet) {
@@ -70,71 +96,119 @@ public CommoditiesForm(UNFPAMIDlet midlet) {
     config = new Configuration();
 
     // creating all fields (blank);
-    reporting_year = new TextField("l'année de déclaration", null, 20, TextField.ANY);
-    reporting_month = new TextField("mois considéré", null, 20, TextField.ANY);
-    location_of_sdp = new TextField("emplacement de sdp", null, 20, TextField.ANY);
-    family_planning = new TextField("planification familiale", null, 20, TextField.ANY);
-    delivery_services = new TextField("services de livraison", null, 20, TextField.ANY);
+    reporting_year = new ChoiceGroup("L'année de déclaration", ChoiceGroup.POPUP, year_list, null);
+    reporting_month = new ChoiceGroup("Mois considéré", ChoiceGroup.POPUP, month_list, null);
+    location_of_sdp = new TextField("Emplacement de sdp", null, 20, TextField.ANY);
 
-    male_condom = new ChoiceGroup("installation fournir préservatif masculin", ChoiceGroup.POPUP, choice, null);
-    female_condom = new ChoiceGroup("installation fournir préservatif féminin", ChoiceGroup.POPUP, choice, null);
-    oral_pills = new ChoiceGroup("installation de fournir la pilule", ChoiceGroup.POPUP, choice, null);
-    injectable = new ChoiceGroup("installation fournir injectable", ChoiceGroup.POPUP, choice, null);
-    iud = new ChoiceGroup("installation fournir DIU", ChoiceGroup.POPUP, choice, null);
-    implants = new ChoiceGroup("installation fournir des implants", ChoiceGroup.POPUP, choice, null);
-    female_sterilization = new ChoiceGroup("installation de fournir la stérilisation féminine", ChoiceGroup.POPUP, choice, null);
-    male_sterilization = new ChoiceGroup("installation de fournir la stérilisation masculine", ChoiceGroup.POPUP, choice, null);
+    family_planning = new ChoiceGroup("Planification familiale", ChoiceGroup.EXCLUSIVE, choice, null);
+    delivery_services = new ChoiceGroup("Services de livraison", ChoiceGroup.EXCLUSIVE, choice, null);
+
+    male_condom = new ChoiceGroup("Préservatif masculin", ChoiceGroup.POPUP, choice, null);
+    nb_male_condom = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    female_condom = new ChoiceGroup("Préservatif féminin", ChoiceGroup.POPUP, choice, null);
+    nb_female_condom = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    oral_pills = new ChoiceGroup("de la pilule", ChoiceGroup.POPUP, choice, null);
+    nb_oral_pills = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    injectable = new ChoiceGroup("Injectable", ChoiceGroup.POPUP, choice, null);
+    nb_injectable = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    iud = new ChoiceGroup("DIU", ChoiceGroup.POPUP, choice, null);
+    nb_iud = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    implants = new ChoiceGroup("des implants", ChoiceGroup.POPUP, choice, null);
+    nb_implants = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    female_sterilization = new ChoiceGroup("La stérilisation féminine", ChoiceGroup.POPUP, yesnoavail, null);
+    nb_female_sterilization = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    male_sterilization = new ChoiceGroup("La stérilisation masculine", ChoiceGroup.POPUP, yesnoavail, null);
+    nb_male_sterilization = new TextField("La quantité", null, 20, TextField.NUMERIC);
     amoxicillin_ij = new ChoiceGroup("amoxicilline injectable disponible", ChoiceGroup.POPUP, choice, null);
+    nb_amoxicillin_ij = new TextField("La quantité", null, 20, TextField.NUMERIC);
     amoxicillin_cap_gel = new ChoiceGroup("amoxicilline capsule disponibles", ChoiceGroup.POPUP, choice, null);
+    nb_amoxicillin_cap_gel = new TextField("La quantité", null, 20, TextField.NUMERIC);
     amoxicillin_suspension = new ChoiceGroup("amoxicilline suspension disponibles", ChoiceGroup.POPUP, choice, null);
+    nb_amoxicillin_suspension = new TextField("La quantité", null, 20, TextField.NUMERIC);
     azithromycine_tab = new ChoiceGroup("L'azithromycine (comprimé / gel).", ChoiceGroup.POPUP, choice, null);
+    nb_azithromycine_tab = new TextField("La quantité", null, 20, TextField.NUMERIC);
     azithromycine_suspension = new ChoiceGroup("suspension d'azithromycine", ChoiceGroup.POPUP, choice, null);
+    nb_azithromycine_suspension = new TextField("La quantité", null, 20, TextField.NUMERIC);
     benzathine_penicillin = new ChoiceGroup("benzathine pénicilline", ChoiceGroup.POPUP, choice, null);
+    nb_benzathine_penicillin = new TextField("La quantité", null, 20, TextField.NUMERIC);
     cefexime = new ChoiceGroup("céfixime disponible", ChoiceGroup.POPUP, choice, null);
+    nb_cefexime = new TextField("La quantité", null, 20, TextField.NUMERIC);
     clotrimazole = new ChoiceGroup("clotrimazole disponible", ChoiceGroup.POPUP, choice, null);
-    ergometrine_tab = new ChoiceGroup("ergométrine (comprimé) disponible", ChoiceGroup.POPUP, choice, null);
-    ergometrine_vials = new ChoiceGroup("ergométrine (flacons) disponible", ChoiceGroup.POPUP, choice, null);
+    nb_clotrimazole = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    ergometrine_tab = new ChoiceGroup("Ergométrine (comprimé) disponible", ChoiceGroup.POPUP, choice, null);
+    nb_ergometrine_tab = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    ergometrine_vials = new ChoiceGroup("Ergométrine (flacons) disponible", ChoiceGroup.POPUP, choice, null);
+    nb_ergometrine_vials = new TextField("La quantité", null, 20, TextField.NUMERIC);
     iron = new ChoiceGroup("fer disponible", ChoiceGroup.POPUP, choice, null);
+    nb_iron = new TextField("La quantité", null, 20, TextField.NUMERIC);
     folate = new ChoiceGroup("acide folique disponibles", ChoiceGroup.POPUP, choice, null);
+    nb_folate = new TextField("La quantité", null, 20, TextField.NUMERIC);
     iron_folate = new ChoiceGroup("fer / acide folique disponibles", ChoiceGroup.POPUP, choice, null);
+    nb_iron_folate = new TextField("La quantité", null, 20, TextField.NUMERIC);
     magnesium_sulfate = new ChoiceGroup("sulfate de magnésium disponible", ChoiceGroup.POPUP, choice, null);
+    nb_magnesium_sulfate = new TextField("La quantité", null, 20, TextField.NUMERIC);
     metronidazole = new ChoiceGroup("métronidazole disponibles", ChoiceGroup.POPUP, choice, null);
-    oxytocine = new ChoiceGroup("l'ocytocine disponible", ChoiceGroup.POPUP, choice, null);
+    nb_metronidazole = new TextField("La quantité", null, 20, TextField.NUMERIC);
+    oxytocine = new ChoiceGroup("L'ocytocine disponible", ChoiceGroup.POPUP, choice, null);
+    nb_oxytocine = new TextField("La quantité", null, 20, TextField.NUMERIC);
 
     append(reporting_year);
     append(reporting_month);
     append(location_of_sdp);
     append(family_planning);
     append(delivery_services);
-
+    append("Les produits fournis par le structure");
     append(male_condom);
+    append(nb_male_condom);
     append(female_condom);
+    append(nb_female_condom);
     append(oral_pills);
+    append(nb_oral_pills);
     append(injectable);
+    append(nb_injectable);
     append(iud);
+    append(nb_iud);
     append(implants);
+    append(nb_implants);
     append(female_sterilization);
+    append(nb_female_sterilization);
     append(male_sterilization);
+    append(nb_male_sterilization);
     append(amoxicillin_ij);
+    append(nb_amoxicillin_ij);
     append(amoxicillin_cap_gel);
+    append(nb_amoxicillin_cap_gel);
     append(amoxicillin_suspension);
+    append(nb_amoxicillin_suspension);
     append(azithromycine_tab);
+    append(nb_azithromycine_tab);
     append(azithromycine_suspension);
+    append(nb_azithromycine_suspension);
     append(benzathine_penicillin);
+    append(nb_benzathine_penicillin);
     append(cefexime);
+    append(nb_cefexime);
     append(clotrimazole);
+    append(nb_clotrimazole);
     append(ergometrine_tab);
+    append(nb_ergometrine_tab);
     append(ergometrine_vials);
+    append(nb_ergometrine_vials);
     append(iron);
+    append(nb_iron);
     append(folate);
+    append(nb_folate);
     append(iron_folate);
+    append(nb_iron_folate);
     append(magnesium_sulfate);
+    append(nb_magnesium_sulfate);
     append(metronidazole);
+    append(nb_metronidazole);
     append(oxytocine);
-    
+    append(nb_oxytocine);
+
     addCommand(CMD_EXIT);
     addCommand(CMD_SEND);
-    addCommand(CMD_SAVE);
     addCommand(CMD_HELP);
 
     this.setCommandListener (this);
@@ -148,11 +222,7 @@ public CommoditiesForm(UNFPAMIDlet midlet) {
 
     public boolean isComplete() {
         // all fields are required to be filled.
-        if (reporting_year.getString().length() == 0  ||
-            reporting_month.getString().length() == 0 ||
-            location_of_sdp.getString().length() == 0 ||
-            family_planning.getString().length() == 0 ||
-            delivery_services.getString().length() == 0)
+        if (location_of_sdp.getString().length() == 0)
             {
             return false;
         }
@@ -166,6 +236,10 @@ public CommoditiesForm(UNFPAMIDlet midlet) {
      */
 
     public boolean isValid() {
+         if (reporting_month.getSelectedIndex() == 0) {
+             ErrorMessage = "Le mois n'est pas valide.";
+             return false;
+        }
         return true;
     }
 
@@ -175,8 +249,45 @@ public CommoditiesForm(UNFPAMIDlet midlet) {
 
     public String toSMSFormat() {
         String sep = " ";
-        return "unfpa stock" + sep + 
-                implants.getSelectedIndex();
+        return "unfpa stock" + sep + reporting_year.getString(reporting_year.getSelectedIndex())
+                             + sep + reporting_month.getSelectedIndex()
+                             + sep + location_of_sdp.getString()
+                             + sep + family_planning.getSelectedIndex()
+                             + sep + delivery_services.getSelectedIndex()
+                             + sep + test_value(male_condom.getSelectedIndex(), nb_male_condom.getString())
+                             + sep + test_value(female_condom.getSelectedIndex(), nb_female_condom.getString())
+                             + sep + test_value(oral_pills.getSelectedIndex(), nb_oral_pills.getString())
+                             + sep + test_value(injectable.getSelectedIndex(), nb_injectable.getString())
+                             + sep + test_value(iud.getSelectedIndex(), nb_iud.getString())
+                             + sep + test_value(implants.getSelectedIndex(), nb_implants.getString())
+                             + sep + test_value(female_sterilization.getSelectedIndex(), nb_female_sterilization.getString())
+                             + sep + test_value(male_sterilization.getSelectedIndex(), nb_male_sterilization.getString())
+                             + sep + test_value(amoxicillin_ij.getSelectedIndex(), nb_amoxicillin_ij.getString())
+                             + sep + test_value(amoxicillin_cap_gel.getSelectedIndex(), nb_amoxicillin_cap_gel.getString())
+                             + sep + test_value(amoxicillin_suspension.getSelectedIndex(), nb_amoxicillin_suspension.getString())
+                             + sep + test_value(azithromycine_tab.getSelectedIndex(), nb_azithromycine_tab.getString())
+                             + sep + test_value(azithromycine_suspension.getSelectedIndex(), nb_azithromycine_suspension.getString())
+                             + sep + test_value(benzathine_penicillin.getSelectedIndex(), nb_benzathine_penicillin.getString())
+                             + sep + test_value(cefexime.getSelectedIndex(), nb_cefexime.getString())
+                             + sep + test_value(clotrimazole.getSelectedIndex(), nb_clotrimazole.getString())
+                             + sep + test_value(ergometrine_tab.getSelectedIndex(), nb_ergometrine_tab.getString())
+                             + sep + test_value(ergometrine_vials.getSelectedIndex(), nb_ergometrine_vials.getString())
+                             + sep + test_value(iron.getSelectedIndex(), nb_iron.getString())
+                             + sep + test_value(folate.getSelectedIndex(), nb_folate.getString())
+                             + sep + test_value(iron_folate.getSelectedIndex(), nb_iron_folate.getString())
+                             + sep + test_value(magnesium_sulfate.getSelectedIndex(), nb_magnesium_sulfate.getString())
+                             + sep + test_value(metronidazole.getSelectedIndex(), nb_metronidazole.getString())
+                             + sep + test_value(oxytocine.getSelectedIndex(), nb_oxytocine.getString());
+    }
+
+    public String test_value(int c, String s) {
+        if (c == 1 && s.length() != 0){
+                System.out.println(c);
+                return s;
+        }
+        else{
+            return "-";
+        }
     }
 
     public void commandAction(Command c, Displayable d) {
@@ -218,6 +329,7 @@ public CommoditiesForm(UNFPAMIDlet midlet) {
             // sends the sms and reply feedback
             SMSSender sms = new SMSSender();
             String number = config.get("server_number");
+            
             if (sms.send(number, this.toSMSFormat())) {
                 alert = new Alert ("Demande envoyée !", "Vous allez recevoir" +
                                    " une confirmation du serveur.",
